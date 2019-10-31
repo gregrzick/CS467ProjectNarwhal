5c6e7968d92fb200e5cd4da6a7b2a47e333ec1ad
Tommy Avetisyan <tomavetisyan95@gmail.com>
2019-10-30 20:42:07 -0700

    Update to Main

--- docs/project.js
+++ docs/project.js
@@ -1,21 +1,29 @@
-// Reference to the canvas element
-let canvas;
-// Context provides functions used for drawing and
-// working with Canvas
-let ctx;
-//Stores previously drawn image data to restore after
-// new drawings are added
-let savedImageData;
-// Stores whether I'm currently dragging the mouse
-let dragging = false;
-let strokeColor = 'black';
-let fillColor = 'black';
-let line_Width = 2;
-let polygonSides = 6;
-// Tool currently using
-let currentTool = 'brush';
-let canvasWidth = 600;
-let canvasHeight = 600;
+//Using canvas
+var canvas;
+//Using canvas libs
+var ctx;
+//Keeps track of current image
+var savedImage;
+//Using brush
+var dragging = false;
+//Default brush color
+var brushColor = 'black';
+//Default line width
+var line_Width=2;
+//Default polygon shapes
+var polygonSides =3;
+//Current tool. Defaut is brush
+var currentTool = 'brush';
+//Drawing boundries
+var canvasWidth = 800;
+var canvasHeight = 800;
+//Toggles drawing
+var drawing = false;
+//Brush x and y points into arrays
+var xPositions;
+var yPositions;
+//If the mouse is clicked down
+var downPos;
 
 // Stores size data used to create rubber band shapes
 // that will redraw as the user moves the mouse
@@ -27,7 +35,6 @@ class ShapeBoundingBox{
         this.height = height;
     }
 }
-
 // Holds x & y position where clicked
 class MouseDownPos{
     constructor(x,y) {
@@ -35,7 +42,6 @@ class MouseDownPos{
         this.y = y;
     }
 }
-
 // Holds x & y location of the mouse
 class Location{
     constructor(x,y) {
@@ -43,7 +49,6 @@ class Location{
         this.y = y;
     }
 }
-
 // Holds x & y polygon point values
 class PolygonPoint{
     constructor(x,y) {
@@ -52,42 +57,294 @@ class PolygonPoint{
     }
 }
 // Stores top left x & y and size of rubber band box
-let shapeBoundingBox = new ShapeBoundingBox(0,0,0,0);
+var shapeBoundingBox = new ShapeBoundingBox(0,0,0,0);
 // Holds x & y position where clicked
-let mousedown = new MouseDownPos(0,0);
+var mousedown = new MouseDownPos(0,0);
 // Holds x & y location of the mouse
-let loc = new Location(0,0);
+var loc = new Location(0,0);
 
 // Call for our function to execute when page is loaded
-document.addEventListener('DOMContentLoaded', setupCanvas);
+document.addEventListener('DOMContentLoaded', Canvas);
 
-function setupCanvas(){
-    // Get reference to canvas element
+function Canvas(){
     canvas = document.getElementById('my-canvas');
-    // Get methods for manipulating the canvas
     ctx = canvas.getContext('2d');
-    ctx.strokeStyle = strokeColor;
+    ctx.strokeStyle = brushColor;
     ctx.lineWidth = line_Width;
-    // Execute ReactToMouseDown when the mouse is clicked
-    canvas.addEventListener("mousedown", ReactToMouseDown);
-    // Execute ReactToMouseMove when the mouse is clicked
-    canvas.addEventListener("mousemove", ReactToMouseMove);
-    // Execute ReactToMouseUp when the mouse is clicked
-    canvas.addEventListener("mouseup", ReactToMouseUp);
+    //Sets backround to white
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //Mouse functions
+    canvas.addEventListener("mousedown", mouseDown);
+    canvas.addEventListener("mousemove", mouseMove);
+    canvas.addEventListener("mouseup", mouseUp);
+}
+function ChangeTool(tool){
+    document.getElementById("brush").className="";
+    document.getElementById("line").className="";
+    document.getElementById("eraser").className="";
+    document.getElementById("text").className="";
+    document.getElementById("circle").className="";
+    document.getElementById("polygon").className="";
+    //Highlight current tool
+    document.getElementById(tool).className="selected";
+    //Change current tool
+    currentTool=tool;
+}
+//Draw with current tool
+function draw(loc){
+    
+    if(currentTool==="brush"){
+        //Draw line
+        DrawBrush();
+    }else if(currentTool==="eraser"){
+        //Draw line, but white
+        DrawBrush();
+    }else if(currentTool==="line"){
+        //Draw straight Line
+        ctx.beginPath();
+        ctx.moveTo(mousedown.x, mousedown.y);
+        ctx.lineTo(loc.x, loc.y);
+        ctx.stroke();
+    } else if(currentTool==="circle"){
+        //Draw circle
+        var radius=shapeBoundingBox.width;
+        ctx.beginPath();
+        ctx.arc(mousedown.x, mousedown.y, radius, 0, Math.PI * 2);
+        ctx.stroke();
+    }  else if(currentTool==="polygon"){
+        //Draw polygons
+        getPolygon();
+        ctx.stroke();
+    }
+}
+function mouseDown(e){
+    xPositions=new Array();
+    yPositions=new Array();
+    downPos=new Array()
+
+    canvas.style.cursor = "crosshair";
+    loc = GetMousePosition(e.clientX, e.clientY);
+    SaveCanvasImage();
+    //Store start positions
+    mousedown.x=loc.x;
+    mousedown.y=loc.y;
+    //Store that the mouse is being held down
+    dragging=true;
+    //Store line points
+    if(currentTool==='brush' || currentTool==='eraser'){
+        drawing = true;
+        storePos(loc.x, loc.y);
+    }
+};
+//Get canvas position
+function GetMousePosition(x,y){
+    var canvasSizeData=canvas.getBoundingClientRect();
+    x=(x-canvasSizeData.left)*(canvas.width/canvasSizeData.width);
+    y=(y-canvasSizeData.top)*(canvas.height/canvasSizeData.height);
+    return {x,y};
+}
+//Store image
+function SaveCanvasImage(){
+    savedImage = ctx.getImageData(0,0,canvas.width,canvas.height);
+}
+//Push array positions
+function storePos(x, y, mouseDown){
+    xPositions.push(x);
+    yPositions.push(y);
+    downPos.push(mouseDown);
+}
+function redrawCanvas(){
+    // Restore image
+    ctx.putImageData(savedImage,0,0);
+}
+function mouseMove(e){
+    canvas.style.cursor = "crosshair";
+    loc = GetMousePosition(e.clientX, e.clientY);
+    //If using the brush or eraser and holding down the mouse, store points
+    if((currentTool==='brush' || currentTool==='eraser') && drawing && dragging){
+        if(currentTool==='brush'){
+            ctx.strokeStyle = document.getElementById("myColor").value;
+        }
+        if(currentTool==='eraser'){
+            ctx.strokeStyle = "white";
+        }
+        //Draw only inside canvas
+        if(loc.x > 0 && loc.x < canvasWidth && loc.y > 0 && loc.y < canvasHeight){
+            storePos(loc.x, loc.y, true);
+        }
+        redrawCanvas();
+        DrawBrush();
+    }else{
+        ctx.strokeStyle = document.getElementById("myColor").value;
+        if(dragging){
+            redrawCanvas();
+            UpdateRubberbandOnMove(loc);
+        }
+    }
+};
+//Connect brush points in the array
+function DrawBrush(){
+    for(var i=1;i<xPositions.length;i++){
+        ctx.beginPath();
+        if(downPos[i]){
+            ctx.moveTo(xPositions[i-1], yPositions[i-1]);
+        }else {
+            ctx.moveTo(xPositions[i]-1, yPositions[i]);
+        }
+        ctx.lineTo(xPositions[i], yPositions[i]);
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+//Reset mouse on mouseup
+function mouseUp(e){
+    canvas.style.cursor = "default";
+    loc = GetMousePosition(e.clientX, e.clientY);
+    redrawCanvas();
+    UpdateRubberbandOnMove(loc);
+    dragging = false;
+    drawing = false;
+}
+function UpdateRubberbandSizeData(loc){
+    // Height & width are the difference between were clicked
+    // and current mouse position
+    shapeBoundingBox.width = Math.abs(loc.x-mousedown.x);
+    shapeBoundingBox.height = Math.abs(loc.y-mousedown.y);
+
+    // If mouse is below where mouse was clicked originally
+    if(loc.x > mousedown.x){
+        // Store mousedown because it is farthest left
+        shapeBoundingBox.left = mousedown.x;
+    } else {
+        // Store mouse location because it is most left
+        shapeBoundingBox.left = loc.x;
+    }
+    // If mouse location is below where clicked originally
+    if(loc.y > mousedown.y){
+
+        // Store mousedown because it is closer to the top
+        // of the canvas
+        shapeBoundingBox.top = mousedown.y;
+    } else {
+        // Otherwise store mouse position
+        shapeBoundingBox.top = loc.y;
+    }
 }
 
-function ChangeTool(toolClicked){
-    document.getElementById("pencil").className = "";
-    document.getElementById("brush").className = "";
-    document.getElementById("line").className = "";
-    document.getElementById("eraser").className = "";
-    document.getElementById("text").className = "";
-    document.getElementById("rectangle").className = "";
-    document.getElementById("circle").className ="";
-    document.getElementById("ellipse").className ="";
-    document.getElementById("polygon").className ="";
-    // Highlight the last selected tool on toolbar
-    document.getElementById(toolClicked).className = "selected";
-    // Change current tool used for drawing
-    currentTool = toolClicked;
+// Returns the angle using x and y
+// x = Adjacent Side
+// y = Opposite Side
+// Tan(Angle) = Opposite / Adjacent
+// Angle = ArcTan(Opposite / Adjacent)
+function getAngle(mouselocX, mouselocY){
+    var adjacent = mousedown.x - mouselocX;
+    var opposite = mousedown.y - mouselocY;
+    return radiansToDegrees(Math.atan2(opposite, adjacent));
+}
+function radiansToDegrees(rad){
+    if(rad < 0){
+        // Correct the bottom error by adding the negative
+        // angle to 360 to get the correct result around
+        // the whole circle
+        return (360.0 + (rad * (180 / Math.PI))).toFixed(2);
+    } else {
+        return (rad * (180 / Math.PI)).toFixed(2);
+    }
+}
+
+//Convert degree to redians
+function degreesToRadians(degrees){
+    degrees=degrees*(Math.PI/180);
+    return degrees;
+}
+function getPolygonPoints(){
+    // Get angle in radians based on x & y of mouse location
+    var angle =  degreesToRadians(getAngle(loc.x, loc.y));
+
+    // X & Y for the X & Y point representing the radius is equal to
+    // the X & Y of the bounding rubberband box
+    var radiusX = shapeBoundingBox.width;
+    var radiusY = shapeBoundingBox.height;
+    // Stores all points in the polygon
+    var polygonPoints = [];
+
+    // Each point in the polygon is found by breaking the
+    // parts of the polygon into triangles
+    // Then I can use the known angle and adjacent side length
+    // to find the X = mouseLoc.x + radiusX * Sin(angle)
+    // You find the Y = mouseLoc.y + radiusY * Cos(angle)
+    for(var i = 0; i < polygonSides; i++){
+        polygonPoints.push(new PolygonPoint(loc.x + radiusX * Math.sin(angle),
+        loc.y - radiusY * Math.cos(angle)));
+
+        // 2 * PI equals 360 degrees
+        // Divide 360 into parts based on how many polygon
+        // sides you want
+        angle += 2 * Math.PI / polygonSides;
+    }
+    return polygonPoints;
+}
+//Draw selected polygon
+function getPolygon(){
+    var polygonPoints = getPolygonPoints();
+    ctx.beginPath();
+    ctx.moveTo(polygonPoints[0].x, polygonPoints[0].y);
+    for(var i = 1; i < polygonSides; i++){
+        ctx.lineTo(polygonPoints[i].x, polygonPoints[i].y);
+    }
+    ctx.closePath();
+}
+function UpdateRubberbandOnMove(loc){
+    // Stores changing height, width, x & y position of most
+    // top left point being either the click or mouse location
+    UpdateRubberbandSizeData(loc);
+    // Redraw the shape
+    draw(loc);
+}
+//Clears page and resets brush points
+function clearCanvas(){
+    canvas.width = canvas.width;
+    xPositions.length = 0;
+    yPositions.length = 0;
+    downPos.length = 0;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+//Changes and displays line width
+function changeLineWidth(slideAmount) {
+    ctx.lineWidth=slideAmount;
+    var sliderDiv = document.getElementById("sliderAmount");
+    sliderDiv.innerHTML = slideAmount;
+}
+//Changes # of polygon sides
+function changeSides(sideAmount){
+    if(sideAmount==='3'){
+        document.getElementById("currentPolygon").src="icons/triangle-icon.png";
+    }
+    if(sideAmount==='4'){
+        document.getElementById("currentPolygon").src="icons/rectangle-icon.png";
+    }
+    if(sideAmount==='5'){
+        document.getElementById("currentPolygon").src="icons/pentagon-icon.png";
+    }
+    if(sideAmount==='6'){
+        document.getElementById("currentPolygon").src="icons/hexagon-icon.png";
+    }
+    polygonSides=sideAmount;
+}
+// Download canvas image as a PNG
+// Can expand later to several buttons to download as different file types
+// Need some function to have user specify a custom filename
+function canvasToPng(){
+  var download = document.getElementById("canvasPNG");
+  //get current canvas
+  var canvas  = document.getElementById("my-canvas");
+  //get actual image data
+  var image   = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+  // send image to download through your browser. Default filename is narwhal-image.
+  download.setAttribute("href", image);
+
+  console.log(image);
 }
